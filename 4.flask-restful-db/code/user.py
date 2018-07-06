e67c959057f1d93d82bef7f1d217e83aa79a7b72
Jasenpan1987 <jasenpan@gmail.com>
2018-07-06 21:53:31 +1000

    test sqlite

---
+++ 4.flask-restful-db/code/user.py
@@ -0,0 +1,5 @@
+class User():
+    def __init__(self, _id, username, password):
+        self.id = _id  # id is a reserved keyword
+        self.username = username
+        self.password = password
